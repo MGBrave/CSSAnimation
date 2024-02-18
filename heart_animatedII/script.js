eea36fedd8d20da6ddb3b7ee6250aceb5e7f9669
M. G <60014891+MGBrave@users.noreply.github.com>
2024-02-18 10:35:54 -0300

    heart animation

---
+++ heart_animatedII/script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "fas fa-heart";
+    heart.style.left = (Math.random() * 100)+"vw";
+    heart.style.animationDuration = (Math.random()*3)+5+"s"
+    body.appendChild(heart);
+}
+setInterval(createHeart,100);
+setInterval(function name(params) {
+    var heartArr = document.querySelectorAll(".fa-heart")
+    if (heartArr.length > 200) {
+       heartArr[0].remove()
+    }
+
+},100)
+
+const year = document.getElementById("year");
+
+year.textContent = new Date().getFullYear();
+
+
